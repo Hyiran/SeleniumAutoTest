cf148d475b339d7c47c18869e98decd2634dfa1c
caijianmin <cajianmin@jd.com>
2016-06-10 17:21:43 +0800

    初始化

--- src/main/java/com/framework/browser/JBrowser.java
+++ src/main/java/com/framework/browser/JBrowser.java
@@ -4,7 +4,9 @@ import com.framework.util.Config;
 import com.framework.util.Constants;
 import com.framework.webdriver.DriverBase;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.RequestHeaders;
 import com.machinepublishers.jbrowserdriver.Settings;
+import com.machinepublishers.jbrowserdriver.UserAgent;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
@@ -19,8 +21,8 @@ public class JBrowser extends DriverBase implements Browser {
         Settings.Builder builder= new Settings.Builder();
         builder.blockAds(true);//是否过滤广告
         builder.javascript(true);//是否支持JS
-        // builder.userAgent(UserAgent.CHROME);
-        // builder.requestHeaders(RequestHeaders.CHROME);
+        builder.userAgent(UserAgent.CHROME);
+        builder.requestHeaders(RequestHeaders.CHROME);
         builder.screen(new Dimension(1920, 1080));
         builder.headless("false".equalsIgnoreCase(config.get("OpenBrowser")));//是否打开浏览器
         builder.quickRender("false".equalsIgnoreCase(config.get("LoadImg")));//配置是否加载图片
